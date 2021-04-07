14e73109be9caecff254398a9b6175575fae5279
oruc <yusa.oruc@stu.fsm.edu.tr>
2021-04-07 17:49:39 +0300

    first commit

---
+++ Assets/Kontrol.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Kontrol : MonoBehaviour
+{
+    public Sprite [] KusSrpite;
+    SpriteRenderer spriteRenderer;
+    bool ileriGeriKontrol = true;
+    int kusSayac = 0;
+    float kusAnimasyonZaman = 0;
+    Rigidbody2D fizik;
+    int puan = 0;
+    bool oyunBitti = true;
+
+    public Text puanText;
+
+    OyunKontrol oyunKontrol;
+
+    AudioSource []sesler;
+
+
+    
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        fizik = GetComponent<Rigidbody2D>();
+        oyunKontrol = GameObject.FindGameObjectWithTag("OyunKontrol").GetComponent<OyunKontrol>();
+        sesler = GetComponents<AudioSource>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && oyunBitti)
+        {
+            fizik.velocity = new Vector2(0, 0);
+            fizik.AddForce(new Vector2(0,200));
+            sesler[0].Play();
+           
+        }
+        if (fizik.velocity.y > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 45);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, -45);
+        }
+        Animasyon();
+
+        
+    }
+    void Animasyon()
+    {
+        kusAnimasyonZaman += Time.deltaTime;
+        if (kusAnimasyonZaman > 0.2f)
+        {
+            kusAnimasyonZaman = 0;
+            if (ileriGeriKontrol)
+            {
+                spriteRenderer.sprite = KusSrpite[kusSayac];
+                kusSayac++;
+                if (kusSayac == KusSrpite.Length)
+                {
+                    kusSayac--;
+                    ileriGeriKontrol = false;
+                }
+            }
+            else
+            {
+                kusSayac--;
+                spriteRenderer.sprite = KusSrpite[kusSayac];
+                if (kusSayac == 0)
+                {
+                    kusSayac++;
+                    ileriGeriKontrol = true;
+                }
+            }
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "puan")
+        {
+            puan++;
+            puanText.text = "Puan = " + puan;
+            sesler[1].Play();
+        }
+        if (collision.gameObject.tag == "engel")
+        {
+            oyunBitti = false;
+            oyunKontrol.oyunBitti();
+            sesler[2].Play();
+            GetComponent<CircleCollider2D>().enabled = false;
+        }
+    }
+}
---
+++ Assets/OyunKontrol.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OyunKontrol : MonoBehaviour
+{
+    public GameObject gokyuzu1;
+    public GameObject gokyuzu2;
+    public float arkaPlanHiz = -1.5f;
+
+    Rigidbody2D fizikbir;
+    Rigidbody2D fizikiki;
+
+    float uzunluk = 0;
+
+    public GameObject engel;
+    public int kacAdetEngel = 5;
+    GameObject[] engeller;
+
+    float degisimZaman = 0;
+    int sayac = 0;
+    bool oyunBittiMi = true;
+    void Start()
+    {
+        fizikbir = gokyuzu1.GetComponent<Rigidbody2D>();
+        fizikiki = gokyuzu2.GetComponent<Rigidbody2D>();
+
+        fizikbir.velocity = new Vector2(arkaPlanHiz, 0);
+        fizikiki.velocity = new Vector2(arkaPlanHiz, 0);
+
+        uzunluk = gokyuzu1.GetComponent<BoxCollider2D>().size.x;
+
+        engeller = new GameObject[kacAdetEngel];
+        for (int i = 0; i < engeller.Length; i++)
+        {
+            engeller[i] = Instantiate(engel, new Vector2(-20, -20), Quaternion.identity);
+            Rigidbody2D fizikEngel = engeller[i].AddComponent<Rigidbody2D>();
+            fizikEngel.gravityScale = 0;
+            fizikEngel.velocity = new Vector2(arkaPlanHiz, 0);
+
+        }
+    }
+
+
+    void Update()
+    {
+        if (oyunBittiMi)
+        {
+
+            if (gokyuzu1.transform.position.x <= -uzunluk)
+            {
+                gokyuzu1.transform.position += new Vector3(uzunluk * 2, 0);
+            }
+            if (gokyuzu2.transform.position.x <= -uzunluk)
+            {
+                gokyuzu2.transform.position += new Vector3(uzunluk * 2, 0);
+            }
+            //----------------------
+            degisimZaman += Time.deltaTime;
+            if (degisimZaman > 2f)
+            {
+                degisimZaman = 0;
+                float Yeksenim = Random.Range(-0.50f, 1.10f);
+                engeller[sayac].transform.position = new Vector3(18, Yeksenim);
+                sayac++;
+                if (sayac >= engeller.Length)
+                {
+                    sayac = 0;
+                }
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    public void oyunBitti()
+    {
+        for (int i = 0; i < engeller.Length; i++)
+        {
+            engeller[i].GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            fizikbir.velocity = Vector2.zero;
+            fizikiki.velocity = Vector2.zero;
+        }
+        oyunBittiMi = false;
+    }
+}
